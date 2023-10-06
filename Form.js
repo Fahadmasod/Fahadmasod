8de26030d406b04c3bfebc00791a4655588c652a
Fahadmasod <147032230+Fahadmasod@users.noreply.github.com>
2023-10-06 09:40:18 +0530

    Add files via upload

---
+++ Form.js
@@ -0,0 +1,76 @@
+import React, { Fragment } from 'react'
+import './App.css';
+
+const Form = () => {
+    return (
+        <Fragment>
+            <div className='Main_Box_Section'>
+                <div className='box1'>
+                    <div className='Box1_sub_section'>
+                        <div className='First_section'>
+                            1
+                        </div>
+                        <div className='section_section'>
+                            2
+                        </div>
+                    </div>
+                    <div className='Box1_sub_section'>
+                        <div className='First_section'>
+                            1
+                        </div>
+                        <div className='section_section'>
+                            2
+                        </div>
+                    </div>
+                    <div className='Box1_sub_section'>
+                        <div className='First_section'>
+                            1
+                        </div>
+                        <div className='section_section'>
+                            2
+                        </div>
+                    </div>
+                    <div className='Box1_sub_section'>
+                        <div className='First_section'>
+                            1
+                        </div>
+                        <div className='section_section'>
+                            2
+                        </div>
+                    </div>
+                    <div className='Box1_sub_section'>
+                        <div className='First_section'>
+                            1
+                        </div>
+                        <div className='section_section'>
+                            2
+                        </div>
+                    </div>
+                    <div className='Box1_sub_section'>
+                        <div className='First_section'>
+                            1
+                        </div>
+                        <div className='section_section'>
+                            2
+                        </div>
+                    </div>
+                </div>
+                <div className='box2'>
+                    <div className='ColumanBox1'>
+                        <div className='ColumanBox1FrstSection'>
+                            1
+                        </div>
+                        <div className='ColumanBox1SecoundSection'>
+                            2
+                        </div>
+                    </div>
+                    <div className='ColumanBox2'>
+                        2
+                    </div>
+                </div>
+            </div>
+        </Fragment>
+    )
+}
+
+export default Form
